f45e2475e0dcfcf33ea7706b308ee2c5be212060
Nathan Cooper <nathanacooper@proton.me>
2026-05-07 16:57:49 -0400

    improvements

--- docxlite/__init__.py
+++ docxlite/__init__.py
@@ -1,8 +1,19 @@
 __version__ = "0.0.2"
 from docx import Document as DocxDocument
 from docx.document import Document
-from docx.text.run import Run
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.table import WD_CELL_VERTICAL_ALIGNMENT, WD_TABLE_ALIGNMENT
+from docx.shared import Emu, Inches, Pt
+from docx.table import Table, _Row, _Cell
+from docx.text.font import Font
 from docx.text.paragraph import Paragraph
-from docx.table import Table
+from docx.text.parfmt import ParagraphFormat
+from docx.text.run import Run
 from docxlite.core import *
-__all__ = ['DocxDocument', 'Paragraph', 'Run', 'Table']
+__all__ = [
+    'DocxDocument', 'set_tracking',
+    'Document', 'Paragraph', 'Run', 'Table', '_Row', '_Cell',
+    'Revision', 'Underline',
+    'Pt', 'Inches', 'Emu',
+    'WD_ALIGN_PARAGRAPH', 'WD_CELL_VERTICAL_ALIGNMENT', 'WD_TABLE_ALIGNMENT',
+]
